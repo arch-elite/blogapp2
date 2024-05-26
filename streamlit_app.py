bba97e72ecabf7cbd2ec09184f71ffc754c1aa7f
arch-elite <168096365+arch-elite@users.noreply.github.com>
2024-05-26 11:25:10 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,17 @@
 import streamlit as st
 import replicate
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 # App title
 st.set_page_config(page_title="Blog Chatbot")
-def configure():
-    load_dotenv()
+#def configure():
+ #   load_dotenv()
 # Replicate Credentials
-configure()
+#configure()
 with st.sidebar:
     st.title('Blog Chatbot')
-    st.write('This chatbot is created to assist you iin any problems met in the blog website.')
+    st.write('This chatbot is created to assist you in any problems met in the blog website.')
     if 'REPLICATE_API_TOKEN' in st.secrets:
         st.success('API key already provided!', icon='✅')
         replicate_api = st.secrets['REPLICATE_API_TOKEN']
@@ -21,7 +21,7 @@ with st.sidebar:
             st.warning('Please enter your credentials!', icon='⚠️')
         else:
             st.success('Proceed to entering your prompt message!', icon='👉')
-    os.environ['REPLICATE_API_TOKEN'] = replicate_api
+    #os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
     st.subheader('Models and parameters')
     selected_model = st.sidebar.selectbox('Choose a Llama2 model', ['Llama2-7B', 'Llama2-13B'], key='selected_model')
@@ -32,7 +32,7 @@ with st.sidebar:
     temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=1.0, value=0.1, step=0.01)
     top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
     max_length = st.sidebar.slider('max_length', min_value=32, max_value=128, value=120, step=8)
-    #st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
+    
 
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
